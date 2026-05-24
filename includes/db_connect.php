eef14403b441be1da652ea130a5eada9ee3eb265
majd <mjdaldynalqadry@gmail.com>
2026-05-24 03:08:24 +0300

    connect to neon db

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'localhost';
-$db   = 'emad_db';
-$user = 'postgres';
-$pass = '1234';
+$host = 'ep-tiny-wave-aqa2wj71-pooler.c-8.us-east-1.aws.neon.tech';
+$db   = 'neondb';
+$user = 'neondb_owner';
+$pass = 'npg_NZPcHVn5UqI8';
 $port = '5432'; // المنفذ الافتراضي لـ PostgreSQL
 
-$dsn = "pgsql:host=$host;port=$port;dbname=$db;";
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
